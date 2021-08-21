4384374e6f5ef0742888fa58460d5060f83e5d20
islamassem95@gmail.com <effSD@qsadasdasda2323!@>
2021-08-21 22:53:19 +0200

    add analysis fragment

--- app/src/main/java/com/banquemisr/nutritionanalysis/features/analysis/AnalyseFragment.kt
+++ app/src/main/java/com/banquemisr/nutritionanalysis/features/analysis/AnalyseFragment.kt
@@ -1,8 +1,64 @@
 package com.banquemisr.nutritionanalysis.features.analysis
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.banquemisr.nutritionanalysis.databinding.FragmentAnalyseBinding
+import com.banquemisr.nutritionanalysis.utils.showToast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AnalyseFragment : Fragment() {
+    private var _binding: FragmentAnalyseBinding? = null
+    private val binding: FragmentAnalyseBinding get() = _binding!!
+    private val viewModel by viewModels<AnalysisViewModel>()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentAnalyseBinding.inflate(inflater).run {
+            _binding = this
+            root
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        viewModel.isLoading.observe(viewLifecycleOwner){
+            binding.progressBar.isVisible = it
+            binding.analyse.isInvisible = it
+        }
+        viewModel.errorMessage.observe(viewLifecycleOwner){ nullableError ->
+            nullableError?.let { error ->
+                val messageRes = error.toString().toIntOrNull()
+                if (messageRes != null)
+                    showToast(messageRes)
+                else showToast(error.toString())
+
+            }
+        }
+        viewModel.recipeIngredients.observe(viewLifecycleOwner){
+            binding.analyse.isEnabled = !it.isNullOrEmpty()
+        }
+        viewModel.analysisResponse.observe(viewLifecycleOwner){
+            it?.let {
+                findNavController().navigate(AnalyseFragmentDirections.showIngredients(it))
+            }
+            binding.progressBar.isVisible = false
+            binding.analyse.isInvisible = false
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/banquemisr/nutritionanalysis/features/analysis/AnalysisViewModel.kt
+++ app/src/main/java/com/banquemisr/nutritionanalysis/features/analysis/AnalysisViewModel.kt
@@ -1,13 +1,93 @@
 package com.banquemisr.nutritionanalysis.features.analysis
 
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.*
+import com.banquemisr.entities.Resource
+import com.banquemisr.entities.postdata.Ingredients
+import com.banquemisr.entities.response.RecipeResponse
+import com.banquemisr.nutritionanalysis.R
+import com.banquemisr.nutritionanalysis.utils.SingleLiveEvent
 import com.banquemisr.usecases.usecases.abstraction.AnalyseUseCase
+import dagger.assisted.Assisted
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 @HiltViewModel
-class AnalysisViewModel @Inject constructor(useCase: AnalyseUseCase) {
+class AnalysisViewModel constructor(
+    private val useCase: AnalyseUseCase,
+    private val ioDispatcher: CoroutineDispatcher,
+    private val mainDispatcher: CoroutineDispatcher
+) : ViewModel() {
+    @Inject
+    constructor(useCase: AnalyseUseCase) : this(useCase, Dispatchers.IO, Dispatchers.Main)
+
+    val analysisResponse by lazy {
+        SingleLiveEvent<RecipeResponse?>()
+    }
+    val errorMessage by lazy {
+        MutableLiveData<Any?>(null)
+    }
+    val isLoading by lazy {
+        SingleLiveEvent<Boolean>()
+    }
     val recipeIngredients by lazy {
-        MutableLiveData<String>()
+        MutableLiveData<String>("")
+    }
+
+    fun analyse() {
+        viewModelScope.launch(ioDispatcher) {
+            val ingredients: List<String> =
+                if (recipeIngredients.value!!.contains("\n"))
+                    recipeIngredients.value!!.split("\n") else listOf(recipeIngredients.value.toString())
+            useCase.getNutritionDetails(Ingredients(ingredients))
+                .catch {
+                    withContext(mainDispatcher) {
+                        errorMessage.value = it.message
+                    }
+                }
+                .collect {
+                    when (it.status) {
+                        Resource.Status.SUCCESS -> {
+                            if (it.data?.error.isNullOrEmpty()) {
+                                withContext(mainDispatcher){
+                                    analysisResponse.value = it.data!!
+                                }
+                            } else {
+                                withContext(mainDispatcher){
+                                    isLoading.value = false
+                                    errorMessage.value = it.data?.error
+                                }
+                            }
+                        }
+                        Resource.Status.LOADING -> {
+                            withContext(mainDispatcher) {
+                                isLoading.value = true
+                            }
+                        }
+                        Resource.Status.ERROR -> {
+                            withContext(mainDispatcher) {
+                                isLoading.value = false
+                                when {
+                                    it.data != null -> {
+                                        errorMessage.value = it.data!!.error
+                                    }
+                                    it.javaClass == UnknownHostException::class.java ->
+                                        errorMessage.value = (
+                                                R.string.no_internet_connection
+                                                )
+                                    else -> errorMessage.value = it.message
+                                }
+                            }
+                        }
+                    }
+                }
+
+        }
     }
 }
